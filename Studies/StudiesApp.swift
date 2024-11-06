5954834e5bfc8165c721105c064598927872a927
adamk <adam@esecure.cc>
2024-11-06 18:53:49 -0500

    Initial Commit

---
+++ Studies/StudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudiesApp.swift
+//  Studies
+//
+//  Created by Adam Kuzma on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudiesTests/StudiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudiesTests.swift
+//  StudiesTests
+//
+//  Created by Adam Kuzma on 11/6/24.
+//
+
+import Testing
+@testable import Studies
+
+struct StudiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
